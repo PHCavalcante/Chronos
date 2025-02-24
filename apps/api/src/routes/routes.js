48af9cc0684c7057f075b9a6723e04b5f7b7e0be
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-02-24 15:12:28 -0300

    refacotr: refactoring codebase to pure js

--- apps/api/src/routes/routes.js
+++ apps/api/src/routes/routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { getEventsController, postEventsController, editEventsController, removeEventsController } from "../controllers/controller";
+import { getEventsController, postEventsController, editEventsController, removeEventsController } from "../controllers/controller.js";
 
 const corsOptions = {
     origin: "http://localhost:3001",
     optionsSuccessStatus: 200,
 }
 
-const routes = (app: express) => {
+const routes = (app) => {
     app.use(express.json());
     app.use(cors(corsOptions));
 
