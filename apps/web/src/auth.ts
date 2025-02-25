4da401573e5f47ad159f0760964938eb229507ca
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-02-25 13:24:34 -0300

    refactor: refactoring auth.ts

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -1,15 +1,15 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthResult } from "next-auth";
 import Github from "next-auth/providers/github";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [Github],
-});
-
-// const result = NextAuth({
+// export const { auth, handlers, signIn, signOut } = NextAuth({
 //   providers: [Github],
 // });
 
-// export const handlers: NextAuthResult["handlers"] = result.handlers;
-// export const auth: NextAuthResult["auth"] = result.auth;
-// export const signIn: NextAuthResult["signIn"] = result.signIn;
-// export const signOut: NextAuthResult["signOut"] = result.signOut;
+const result = NextAuth({
+  providers: [Github],
+});
+
+export const handlers: NextAuthResult["handlers"] = result.handlers;
+export const auth: NextAuthResult["auth"] = result.auth;
+export const signIn: NextAuthResult["signIn"] = result.signIn;
+export const signOut: NextAuthResult["signOut"] = result.signOut;
